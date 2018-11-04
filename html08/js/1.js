7e4c4bebfcb269714fa0a991d74a0cb50ffff569
Tuoithotranve98 <nxtho0109@gmail.com>
2018-11-04 20:45:46 +0700

    tho

---
+++ html08/js/1.js
@@ -0,0 +1,28 @@
+function clickMe(){
+	// alert('Chào ekko')
+	// .....
+	// if (confirm('Are yeu sure')) {
+	// 	window.location.href="https://google.com"
+	// }
+	// .....
+	// var a = prompt('Input a')
+	// var b = prompt('Input b')//tra ve gia tri nguoi dung nhap vao
+	// alert(parseInt(a)+ parseInt(b))
+	// ....
+	// document.write(parseInt(a)+ parseInt(b))
+	// document.getElementById('name').style.color='red';
+	// ....
+	// document.getElementById('name').innerHTML='Ekko dep zai'
+	// ...
+	// var a=document.getElementsByClassName('text');
+	// for(var i=a.length-1; i>=0;i--)
+	// 	{
+ //    		a[i].style.color="red" 
+	// 	}
+	// ...
+	var a=document.getElementsByTagName('p');
+	for(var i=a.length-1; i>=0;i--)
+		{
+    		a[i].style.color="red" 
+		}
+}
